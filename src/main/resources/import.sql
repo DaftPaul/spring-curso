136a5a2bbb0430ec3a55134283958c2f127be0e8
DaftPaul <paul.axel.tang.arce.st@everis.com>
2019-02-13 00:33:47 -0500

    first commit

---
+++ src/main/resources/import.sql
@@ -0,0 +1,2 @@
+INSERT INTO clientes (id, nombre, apellido, email, create_at) VALUES (1, 'Andres', 'Guzman', 'profesando@ctmre.com', '2018-04-03');
+INSERT INTO clientes (id, nombre, apellido, email, create_at) VALUES (2, 'Lalo', 'Salomon', 'profesan999221do@ctmre.com', '2015-04-03');
